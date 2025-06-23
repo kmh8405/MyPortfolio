4f302aed6729769bc3cc0944d841ec86493a1bb3
kmh8405 <kmh.ssem8405@gmail.com>
2025-06-23 21:31:19 +0900

    Update projects.ts

--- src/components/data/projects.ts
+++ src/components/data/projects.ts
@@ -5,6 +5,7 @@ import Project4 from "../../assets/Project4.png";
 import Project5 from "../../assets/Project5.png";
 import Project6 from "../../assets/Project6.png";
 import Project7 from "../../assets/Project7.png";
+import Project8 from "../../assets/Project8.png";
 
 export interface ProjectData {
   id: number;
